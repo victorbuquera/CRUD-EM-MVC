438a552723ac824601e836248907bb379d876972
victorbuquera <victorbuquera@gmail.com>
2022-03-10 14:41:47 -0300

    ok

--- app/Controllers/Site.php
+++ app/Controllers/Site.php
@@ -4,5 +4,13 @@ namespace app\Controllers;
 
 class Site
 {
-    
+    public function home()
+    {
+        require_once __DIR__ .'/../Views/home.php';
+    }
+
+    public function galeria()
+    {
+        require_once __DIR__ .'/../Views/galeria.php';
+    }
 }
