cb6d2ee3e5bf2afd611c57519e9491e5d8ea5953
victorbuquera <victorbuquera@gmail.com>
2022-03-10 12:06:33 -0300

    Sistema de rotas configurado

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso mvc</title>
+
+    <link rel="stylesheet" href="config/materialize/css/materialize.min.css">
+</head>
+<body>
+    
+    <nav></nav>
+
+    <script src="config/materialize/js/materialize.min.js"></script>
+</body>
+</html>
